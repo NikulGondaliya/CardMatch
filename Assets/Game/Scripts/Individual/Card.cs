87d4f92ef62d0f14af416561c0efe903460f14eb
Nikul Gondaliya <95203720+NikulGondaliya@users.noreply.github.com>
2024-04-25 22:12:27 +0530

    make flip functionality

---
+++ Assets/Game/Scripts/Individual/Card.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public Sprite backSprite;
+    public Sprite frontSprite;
+
+    private Image Image;
+    private float cardfliptime;
+    private bool isHide = true;
+    private bool isClick = false;
+
+    private Transform transform;
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+        Image = GetComponent<Image>();
+        GetComponent<Button>().onClick.AddListener(() => this.OnClick());
+
+    }
+
+    private void Start()
+    {
+        Image.sprite = backSprite;
+    }
+
+    public void OnClick()
+    {
+        if(isClick) return;
+        
+        isClick = true;
+
+        if (isHide)
+            StartCoroutine(OpenCard());
+        else
+            StartCoroutine(CloseCard());
+
+    }
+
+    private IEnumerator OpenCard()
+    {
+        for (int i = 0; i < 90; i++)
+        {
+            yield return new WaitForSeconds(cardfliptime / 90);
+            transform.rotation = Quaternion.Euler(0, i, 0);
+        }
+
+        Image.sprite = frontSprite;
+        for (int i = 90; i >= 0; i--)
+        {
+            yield return new WaitForSeconds(cardfliptime / 90);
+            transform.rotation = Quaternion.Euler(0, i, 0);
+        }
+        isHide = false;
+        isClick=false;
+    }
+
+    private IEnumerator CloseCard()
+    {
+        for (int i = 0; i < 90; i++)
+        {
+            yield return new WaitForSeconds(cardfliptime / 90);
+            transform.rotation = Quaternion.Euler(0, i, 0);
+        }
+        Debug.Log("BackSprite");
+        Image.sprite = backSprite;
+        for (int i = 90; i >= 0; i--)
+        {
+            yield return new WaitForSeconds(cardfliptime / 90);
+            transform.rotation = Quaternion.Euler(0, i, 0);
+        }
+        isHide = true;
+        isClick = false;
+    }
+
+
+}
